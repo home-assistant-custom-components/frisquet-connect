0810618142ea127cd333221a13dce094a361405c
Bruno TÊTE <bruno.tete@gmail.com>
2025-02-13 20:48:24 +0100

    fix

--- custom_components/frisquet_connect_unofficial/entities/utils.py
+++ custom_components/frisquet_connect_unofficial/entities/utils.py
@@ -9,6 +9,8 @@ from custom_components.frisquet_connect_unofficial.devices.frisquet_connect_coor
 # https://developers.home-assistant.io/docs/device_registry_index/
 def get_device_info(name: str, unique_id: str, coordinator: FrisquetConnectCoordinator) -> DeviceInfo:
     return DeviceInfo(
+        identifiers={(DOMAIN, unique_id)},
+        name=name,
         manufacturer=DEVICE_MANUFACTURER,
         model=coordinator.site.product,
         serial_number=coordinator.site.serial_number,
